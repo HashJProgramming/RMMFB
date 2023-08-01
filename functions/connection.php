cb8de10df7e82f67bf3dd20c8ba6a3330d504c04
HashJProgramming <joshuamarceloambalong@gmail.com>
2023-08-02 05:17:31 +0800

    Version 1.2 - Functions Added

---
+++ functions/connection.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'setup.php';
+$db = new PDO('mysql:host=localhost;dbname=' . $database, 'root', '');
+
+if (!$db) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+function generate_logs($type, $logs)
+{
+    session_start();
+    global $db;
+    $sql = "INSERT INTO logs (user_id, logs, type) VALUES (:user_id, :logs, :type)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':user_id', $_SESSION['id']);
+    $stmt->bindParam(':logs', $logs);
+    $stmt->bindParam(':type', $type);
+    $stmt->execute();
+}
---
+++ functions/setup.php
@@ -0,0 +1,80 @@
+<?php
+    $database = 'rmmfb';
+    $db = new PDO('mysql:host=localhost', 'root', '');
+    $query = "CREATE DATABASE IF NOT EXISTS $database";
+
+    try {
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->exec($query);
+        $db->exec("USE $database");
+
+        $db->exec("
+            CREATE TABLE IF NOT EXISTS users (
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              username VARCHAR(255),
+              password VARCHAR(255),
+              created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ");
+
+        $db->exec("
+            CREATE TABLE IF NOT EXISTS customers (
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              fullname VARCHAR(255),
+              address VARCHAR(255),
+              phone VARCHAR(255),
+              email VARCHAR(255),
+              user_id int,
+              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+              created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ");
+
+        $db->exec("
+            CREATE TABLE IF NOT EXISTS transactions (
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              customer_id int,
+              item VARCHAR(255),
+              price DECIMAL(10,2),
+              borrow DATE,
+              return DATE,
+              penalty DECIMAL(10,2),
+              condition VARCHAR(255),
+              status VARCHAR(255),
+              FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
+              created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ");
+
+        $db->exec("
+          CREATE TABLE IF NOT EXISTS logs (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            user_id int,
+            logs TEXT,
+            type TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+        ");
+
+        $db->beginTransaction();
+
+        $stmt = $db->prepare("SELECT COUNT(*) FROM `users` WHERE `username` = 'admin'");
+        $stmt->execute();
+        $userExists = $stmt->fetchColumn();
+        
+        if (!$userExists) {
+            $stmt = $db->prepare("INSERT INTO `users` (`username`, `password`) VALUES (:username, :password)");
+            $stmt->bindValue(':username', 'admin');
+            $stmt->bindValue(':password', '$2y$10$WgL2d2fzi6IiGiTfXvdBluTLlMroU8zBtIcRut7SzOB6j9i/LbA4K');
+            $stmt->bindValue(':level', 0);
+            $stmt->execute();
+        }
+        
+        $db->commit();
+
+    } catch(PDOException $e) {
+        die("Error creating database: " . $e->getMessage());
+    }
+    $db = null;
+?>
