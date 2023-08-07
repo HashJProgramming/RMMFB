013a2c849d0c397bde7fa2dd634683742727323e
HashJProgramming <joshuamarceloambalong@gmail.com>
2023-08-07 09:43:15 +0800

    Version 2.3 - bug fix

--- rents.php
+++ rents.php
@@ -58,7 +58,7 @@ include_once 'functions/view/nav-bar.php';
                         <div class="card-body">
                             <div class="row align-items-center no-gutters">
                                 <div class="col me-2">
-                                    <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>TOTAL LATE</span></div>
+                                    <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>TOTAL OVERDUE</span></div>
                                     <div class="text-dark fw-bold h5 mb-0"><span><?php echo get_total_late() ?></span></div>
                                 </div>
                                 <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
