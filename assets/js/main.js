f746318aa3b7a8e021c78d0ed59e6363971cccca
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2024-03-30 09:50:37 +0800

    Update background animation in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,19 +47,19 @@ $('#dataTable').DataTable( {
     ]
 } );
 
-// VANTA.WAVES({
-//     el: "#bg-animation",
-//     mouseControls: false,
-//     touchControls: true,
-//     gyroControls: false,
-//     minHeight: 200.00,
-//     minWidth: 200.00,
-//     scale: 1.00,
-//     scaleMobile: 1.00,
-//     color: 0xb7b7c0,
-//     waveSpeed: 1.00,
-//     zoom: 0.60
-//   })  
+VANTA.WAVES({
+    el: "#bg-animation",
+    mouseControls: false,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xb7b7c0,
+    waveSpeed: 1.00,
+    zoom: 0.60
+  })  
     
     if (type == 'success') {
         Swal.fire(
